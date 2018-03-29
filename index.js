20f903e695ad02440b1f8c4e7636853a690a048f
bilabila <bilabila@qq.com>
2018-03-29 23:55:43 +0800

    rate2

--- index.js
+++ index.js
@@ -23,63 +23,71 @@ const settings = {
     minRate: 1,
     step: 0.5
 }
-const selectors = {
-    videoDiv: 'div.j-unitctBox.unitctBox.f-pr',
-    video: 'video',
-    sbg: 'div.sbg',
-    bbg: 'div.bbg',
-    source: 'video > source',
-    volumnBtn: 'div.controlbar_btn.volumebtn.j-volumebtn',
-    rateBtn: 'div.controlbar_btn.ratebtn.j-ratebtn',
-    rateTxt: 'span.ratebtn_text.j-ratebtn_text',
-}
-const nodes = {}
-const waitForAll = () => new Promise(resolve => {
-    const delay = 500
-    const f = () => {
-        Object.entries(selectors).forEach(i => nodes[i[0]] = document.querySelector(i[1]))
-        if (Object.values(nodes).every(v => v != null)) {
-            resolve()
-        } else {
-            setTimeout(f, delay)
+const icourse163 = {
+    selectors: {
+        videoDiv: 'div.j-unitctBox.unitctBox.f-pr',
+        video: 'video',
+        sbg: 'div.sbg',
+        bbg: 'div.bbg',
+        source: 'video > source',
+        volumnBtn: 'div.controlbar_btn.volumebtn.j-volumebtn',
+        rateBtn: 'div.controlbar_btn.ratebtn.j-ratebtn',
+        rateTxt: 'span.ratebtn_text.j-ratebtn_text',
+    },
+    nodes: {},
+    waitForAll() {
+        return new Promise(resolve => {
+            const delay = 500
+            const f = () => {
+                Object.entries(this.selectors).forEach(i => this.nodes[i[0]] = document.querySelector(i[1]))
+                if (Object.values(this.nodes).every(v => v != null)) {
+                    resolve()
+                } else {
+                    setTimeout(f, delay)
+                }
+            }
+            f()
+        })
+    },
+    setRate(rate) {
+        console.log('set');
+        const { maxRate, minRate } = settings
+        if (rate > maxRate || rate < minRate) rate = minRate
+        GM_setValue('rate', rate)
+        settings.currentRate = rate
+        this.nodes.video.playbackRate = rate
+        this.nodes.rateTxt.innerHTML = 'x' + rate.toFixed(1)
+    },
+    initial: true,
+    override() {
+        const { currentRate, step } = settings
+        const { video, rateBtn, rateTxt } = this.nodes
+        if (this.initial) {
+            this.setRate(currentRate)
+            this.initial = false
         }
+        rateBtn.removeChild(rateBtn.querySelector('div'))
+        rateTxt.addEventListener('click', () => {
+            this.setRate(currentRate + step)
+        })
+        video.addEventListener('playing', () => {
+            this.setRate(currentRate)
+        })
+        video.removeAttribute('autoplay')
+        video.setAttribute('preload', 'auto')
+        video.play()
+    },
+    init() {
+        this.waitForAll().then(() => {
+            this.override()
+            const config = { attributes: true, childList: true }
+            const callback = async () => {
+                await this.waitForAll()
+                this.override()
+            }
+            const observer = new MutationObserver(callback)
+            observer.observe(this.nodes.videoDiv, config)
+        })
     }
-    f()
-})
-const setRate = (rate) => {
-    const { maxRate, minRate } = settings
-    if (rate > maxRate || rate < minRate) rate = minRate
-    GM_setValue('rate', rate)
-    settings.currentRate = rate
-    nodes.video.playbackRate = rate
-    nodes.rateTxt.innerHTML = 'x' + rate.toFixed(1)
 }
-let init = true
-const override = () => {
-    const { currentRate, step } = settings
-    const { video, volumnBtn, rateBtn, rateTxt } = nodes
-    if (init) {
-        setRate(currentRate)
-        init = false
-    }
-    rateBtn.removeChild(rateBtn.querySelector('div'))
-    rateTxt.addEventListener('click', () => {
-        setRate(currentRate + step)
-    })
-    video.addEventListener('playing', () => {
-        setRate(currentRate)
-    })
-    video.removeAttribute('autoplay')
-    video.setAttribute('preload', 'auto')
-    video.play()
-}
-waitForAll().then(_ => {
-    override()
-    const config = { attributes: true, childList: true }
-    const callback = async () => {
-        await waitForAll()
-        override()
-    }
-    const observer = new MutationObserver(callback)
-    observer.observe(nodes.videoDiv, config)
-})
+icourse163.init()
